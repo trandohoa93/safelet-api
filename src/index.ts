a1a1bf6d09e67727ecebde369b67964394c07979
Tran Do Hoa <trandohoa93@gmail.com>
2023-07-07 17:14:57 +0700

    add folder api

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,15 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
+app.get('/api', (req: Request, res: Response) => {
+  res.send('Hello this is API Router');
+});
+
+app.get('/api/:id', (req: Request, res: Response) => {
+  console.log(req.params.id)
+  res.send('Hello this is API Router');
+});
+
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
