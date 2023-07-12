e1890170401e65bacabdf0abfac694d66205c2ef
Tran Do Hoa <trandohoa93@gmail.com>
2023-07-12 11:16:13 +0700

    add module alarms, users

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,15 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import {router as users} from './modules/users';
+import {router as alarms} from './modules/alarms';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
-
-app.get('/api', (req: Request, res: Response) => {
-  res.send('Hello this is API Router');
-});
-
-app.get('/api/:id', (req: Request, res: Response) => {
-  console.log(req.params.id)
-  res.send('Hello this is API Router');
-});
+app.use('/api/users', users);
+app.use('/api/alarms', alarms)
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
